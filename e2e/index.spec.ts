409a35b6f20dd52e6b51b5c78aa1f82042d4dec8
Glasabarn <viggoh21@ru.is>
2025-03-02 22:27:06 +0000

    Didn't give myself enough time for this assignment :/

--- e2e/index.spec.ts
+++ e2e/index.spec.ts
@@ -10,3 +10,51 @@ test("should navigate to index page and have correct title", async ({
   // The page should contain a title element with the text "TODO 📃"
   await expect(page.title()).resolves.toMatch("TODO 📃");
 });
+
+
+test("should ensure the TODO list is empty", async ({
+  page,
+}) => {
+  // Go to the index page
+  await page.goto("/");
+  // Construct a counter and count list items
+  await expect(page.locator("li")).toHaveCount(0);
+
+});
+
+
+test("should add a new item to the TODO list", async ({ page }) => {
+  // Go to the index page
+  await page.goto("/");
+
+  // Locate the input field and type a new TODO item
+  await page.fill("input", "Test Item"); // Update the selector as needed
+
+  // Click the "Add" button
+  await page.click("button[type='submit']"); // Update the selector as needed
+
+  // Verify that the new TODO item appears in the list
+  await expect(page.locator("li")).toContainText("Test Item");
+});
+
+
+test("should add a second TODO item to the list", async ({ page }) => {
+  // Go to the index page
+  await page.goto("/");
+
+  // Locate the input field and type a new TODO item
+  await page.fill("input", "Test Item2");
+
+  // Click the "Add" button
+  await page.click("button[type='submit']");
+
+  // Verify that the new TODO item appears in the list
+  await expect(page.locator("li")).toContainText("Test Item2");
+  
+  // Verify that there are two TODO items
+  await expect(page.locator("li")).toHaveCount(2);
+
+});
+
+
+
